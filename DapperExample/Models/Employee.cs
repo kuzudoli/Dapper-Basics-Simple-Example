369cb464b52b4cb33270e5a68bf21ac85eff1635
kuzudoli <enescakkr@windowslive.com>
2023-10-10 16:47:44 +0300

    Add project files.

---
+++ DapperExample/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace DapperExample.Models
+{
+    public class Employee
+    {
+        public int employee_id { get; set; }
+        public string? first_name { get; set; }
+        public string? last_name { get; set; }
+        public int age { get; set; }
+        public string? email { get; set; }
+        public string? postal_code { get; set; }
+        public string? favorite_color { get; set; }
+        public string? hire_date { get; set; }
+        public double salary { get; set; }
+    }
+}
---
+++ DapperExample/Program.cs
@@ -0,0 +1,80 @@
+﻿using Dapper;
+using DapperExample.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+await GetList();
+
+async Task GetList()
+{
+    using (IDbConnection conn = new SqlConnection("Data Source=.;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=30;"))
+    {
+        #region QueryAsync (Select)
+        var model = await conn.QueryAsync<Employee>("SELECT * FROM Employees");
+        #endregion
+
+        #region ExecuteAsync (Insert)
+        //var hasData = conn.QueryAsync<Employee>("SELECT * FROM Employees WHERE first_name = 'Enes'").Result.Any(x => x.first_name == "Enes");
+        //if (!hasData)
+        //{
+        //    var result = await conn.ExecuteAsync("INSERT INTO Employees Values(3278,'Enes','Çakır',23,'enesckr@gmail.com','Turkey','28000','Red','01/08/2021','10000')");
+        //    var response = await conn.QueryAsync<Employee>("SELECT * FROM Employees WHERE first_name = 'Enes'");
+        //}
+        #endregion
+
+        #region ExecuteAsync (Update)
+        //var result = await conn.ExecuteAsync("UPDATE Employees SET favorite_color = 'Yellow' WHERE employee_id = '3278'");
+        //var response = await conn.QueryAsync<Employee>("SELECT * FROM Employees WHERE first_name = 'Enes'");
+        #endregion
+
+        #region ExecuteAsync (Delete)
+        //var result = await conn.ExecuteAsync("DELETE FROM Employees WHERE age < 19");
+        #endregion
+
+
+        #region QueryAsync (SP-Select)
+
+        #region SQL Komutu: GetYoungEmployeesByFavoriteColor
+        /*CREATE PROCEDURE GetYoungEmployeesByFavoriteColor
+        (@age int, @favColor varchar(50))
+        AS
+        BEGIN
+	        SELECT * FROM Employees WHERE age < @age AND favorite_color = @favColor
+        END*/
+        #endregion
+
+        //var parameters = new DynamicParameters();
+        //parameters.Add("age", 23);
+        //parameters.Add("favColor", "blue");
+
+        //var result = await conn.QueryAsync("GetYoungEmployeesByFavoriteColor", parameters, null, null, CommandType.StoredProcedure);
+        //var result2 = await conn.QueryAsync("GetYoungEmployeesByFavoriteColor", new { age = 25, favColor = "red" }, null, null, CommandType.StoredProcedure);
+        #endregion
+
+        #region ExecuteAsync (SP-Insert)
+
+        #region SQL Komutu: InsertEmployee
+
+        /*CREATE PROCEDURE InsertEmployee
+        (@id int,@name varchar,@lastname varchar, @country varchar, @age int, @favColor varchar)
+        AS
+        BEGIN
+	        INSERT INTO Employees(employee_id, first_name, last_name, country, age, favorite_color) VALUES(@id,@name,@lastname,@country, @age, @favColor);
+        END*/
+        #endregion
+
+        //var parameters = new DynamicParameters();
+        //parameters.Add("id", 3279);
+        //parameters.Add("name", "Ahmet");
+        //parameters.Add("lastname", "Sevgi");
+        //parameters.Add("country", "Turkey");
+        //parameters.Add("age", 26);
+        //parameters.Add("favColor", "black");
+
+        //var result = await conn.ExecuteAsync("InsertEmployee", parameters, null, null, CommandType.StoredProcedure);
+        #endregion
+
+        //Breakpoint variable for debugging the results
+        var wait = 1;
+    }
+}
